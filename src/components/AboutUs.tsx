79aa954f61308f5adaaf500f399a96061f307935
vishwajeet <singhvishwajeet1998@gmail.com>
2022-07-21 23:36:30 +0530

    order confirmation and about us static pages

---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,19 @@
+const AboutUs = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut tenetur porro
+      cumque laudantium delectus veritatis repellendus corporis amet magni iure,
+      asperiores praesentium accusamus eveniet at error! Nulla nisi expedita, a
+      molestiae et perspiciatis suscipit unde quo mollitia reprehenderit tempora
+      consequuntur modi officia quasi illo sequi laudantium repellat in non quis
+      tenetur! Corporis consectetur nihil omnis hic, veritatis non praesentium
+      quod dolorem dolor, provident eius necessitatibus ut. Aperiam a rerum
+      distinctio veniam deserunt obcaecati omnis reiciendis alias deleniti eos
+      tempora, odio similique sed minus voluptatibus at adipisci, necessitatibus
+      unde vitae atque, aliquid error inventore ratione. Eum debitis delectus in
+      iusto earum?
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -1,11 +1,25 @@
+import moment from "moment";
+
 const OrderConfirmation = (props: any) => {
   const orderID = props.match.params.id;
-  const orderData = JSON.parse(localStorage.getItem("cart")!);
-  const filteredData = orderData?.filter(
+  const orderData = JSON.parse(localStorage.getItem("cart")!)?.filter(
     (data: any) => data?.id === Number(orderID)
   );
   console.log(orderData);
-  return <div>{JSON.stringify(filteredData)}</div>;
+  return (
+    <div>
+      <h2>Order Confirmed!</h2>
+      <div className="item" key={orderData[0]?.id}>
+        <img src={orderData[0]?.image} alt="" />
+        <p>
+          Total : {orderData[0]?.quantity} X {orderData[0]?.price} ={" "}
+          {orderData[0]?.total}
+        </p>
+        <p>{orderData.category}</p>
+        <p>Date of delivery : </p> {moment(new Date()).toISOString()}
+      </div>
+    </div>
+  );
 };
 
 export default OrderConfirmation;
