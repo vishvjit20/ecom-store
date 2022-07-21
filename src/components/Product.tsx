41627faceea158b3d384c720dc65508ed34d2315
vishwajeet <singhvishwajeet1998@gmail.com>
2022-07-21 22:49:36 +0530

    add to cart local storage implementation

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,8 +1,56 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import "./product.css";
 
 const Product = () => {
   const product = useSelector((state: any) => state?.products?.product);
+  const [quantity, setQuantity] = useState<number>(1);
+  const MAX_QUANTITY = 50;
+  const increaseQuantity = () => {
+    if (quantity < MAX_QUANTITY) setQuantity(quantity + 1);
+  };
+  const reduceQuantity = () => {
+    if (quantity > 1) setQuantity(quantity - 1);
+  };
+  const addToCart = () => {
+    const cartItem = JSON.parse(localStorage.getItem("cart")!);
+    const currentCartValue = cartItem?.filter(
+      (item: any) => item.id === product?.id
+    );
+
+    const remainingCartItems =
+      cartItem?.filter((item: any) => item?.id !== product?.id) || [];
+    // debugger;
+    if (currentCartValue?.length > 0) {
+      let newQuantity: number = quantity + currentCartValue[0]?.quantity;
+      let cartObj = {
+        id: product?.id,
+        quantity: newQuantity,
+        category: product?.category,
+        price: product?.price,
+        total: newQuantity * product?.price,
+        image: product?.image,
+      };
+      localStorage.setItem(
+        "cart",
+        JSON.stringify([...remainingCartItems, cartObj])
+      );
+    } else {
+      const newQuantity = quantity ? quantity : 1;
+      let cartObj = {
+        id: product?.id,
+        quantity: newQuantity,
+        category: product?.category,
+        price: product?.price,
+        total: newQuantity * product?.price,
+        image: product?.image,
+      };
+      localStorage.setItem(
+        "cart",
+        JSON.stringify([...remainingCartItems, cartObj])
+      );
+    }
+  };
   return (
     <div className="product">
       <div className="product-left">
@@ -18,11 +66,11 @@ const Product = () => {
         <div>
           <p>Rating : {product?.rating?.rate}</p>
           <p>Quantity : </p>
-          <button>-</button>
-          {1}
-          <button>+</button>
+          <button onClick={reduceQuantity}>-</button>
+          {quantity}
+          <button onClick={increaseQuantity}>+</button>
         </div>
-        <button>Add To Cart</button>
+        <button onClick={addToCart}>Add To Cart</button>
       </div>
     </div>
   );
